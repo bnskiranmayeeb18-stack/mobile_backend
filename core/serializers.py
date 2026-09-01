ff774c5638acddf8e57909cd3c6b0e46b1bc1aeb
bnskiranmayeeb18@gmail.com <bnskiranmayeeb18@gmail.com>
2026-09-01 16:18:52 +0530

    EPIC 03 - All APIs & Validation DONE

--- core/serializers.py
+++ core/serializers.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework import serializers
 from .models import DriverProfile, Vehicle, VehicleType, Ride, RideLocation
 
@@ -16,12 +17,28 @@ class VehicleSerializer(serializers.ModelSerializer):
         model = Vehicle
         fields = '__all__'
 
-class RideLocationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RideLocation
-        fields = '__all__'
+    def validate_vehicle_number(self, value):
+        pattern = r'^[A-Z]{2}[0-9]{1,2}[A-Z]{1,2}[0-9]{4}$'
+        clean_value = value.replace(" ", "").upper()
+        if not re.match(pattern, clean_value):
+            raise serializers.ValidationError("Invalid vehicle registration number. Eg: AP05AB1234")
+        return clean_value
+
+    def validate(self, data):
+        if self.instance is None: # Create time lo mathrame duplicate check
+            if Vehicle.objects.filter(
+                driver=data.get('driver'),
+                vehicle_number=data.get('vehicle_number')
+            ).exists():
+                raise serializers.ValidationError("Duplicate vehicle for this driver")
+        return data
 
 class RideSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ride
+        fields = '__all__'
+
+class RideLocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RideLocation
         fields = '__all__'
