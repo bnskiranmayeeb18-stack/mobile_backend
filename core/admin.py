52afc6e95f064b9cee8d9efe37143e7962bc0473
bnskiranmayeeb18@gmail.com <bnskiranmayeeb18@gmail.com>
2026-09-08 14:37:02 +0530

    feat(epic-03): Task 1 - DriverLocation model and admin - Kiranmayee - 24th Aug work completed on 08-09-2026

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from.models import Ride
+from .models import Ride, DriverLocation
 
-admin.site.register(Ride)
+@admin.register(Ride)
+class RideAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'fare', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['customer__username']
+
+@admin.register(DriverLocation)
+class DriverLocationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'driver', 'latitude', 'longitude', 'accuracy', 'updated_at']
+    list_filter = ['updated_at']
--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from.models import Ride
+from .models import Ride, DriverLocation
 
 class RideSerializer(serializers.ModelSerializer):
+    customer_username = serializers.CharField(source='customer.username', read_only=True)
     class Meta:
         model = Ride
-        fields = '__all__'
-        read_only_fields = ['customer']
+        fields = ['id', 'customer', 'customer_username', 'pickup_location', 'drop_location', 'fare', 'created_at']
+
+class DriverLocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DriverLocation
+        fields = ['id', 'driver', 'latitude', 'longitude', 'accuracy', 'updated_at']
